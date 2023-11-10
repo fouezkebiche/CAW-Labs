51e1b07653612f497a3fd028b5c84a5f298b80a4
fouezkebiche <130800462+fouezkebiche@users.noreply.github.com>
2023-11-10 11:45:57 -0800

    Add files via upload

---
+++ CAW_Lab3.part1_Gr2_kebiche&benghalia/fileImport.js
@@ -0,0 +1,9 @@
+const mean = require('./notation'); 
+
+const scores1 = [85, 90, 92, 88, 78];
+const average1 = mean(scores1);
+console.log('Average of scores1:', average1);
+
+const scores2 = [75, 80, 92, 95, 88];
+const average2 = mean(scores2);
+console.log('Average of scores2:', average2);
